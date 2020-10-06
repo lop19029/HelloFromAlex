ba446a5ea55360588e842098e6f4d98cd506c021
James Adams <44241467+Jadams2013@users.noreply.github.com>
2020-10-06 16:42:25 -0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello my name is Alex");
+        System.out.println("Hello Alex, my name is James.");
     }
 }
